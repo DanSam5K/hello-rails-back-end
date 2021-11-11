a5bc3eba401191a8b8baedaf62bab3152c2b5acd
Daniel Samuel <dansam5000@gmail.com>
2021-11-11 23:21:44 +0100

    fix: Rubocop and linters errors

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -1,6 +1,6 @@
 class GreetingController < ApplicationController
   # GET /greeting
   def index
-	  render json: Greeting.all.sample, only: [:message]
+    render json: Greeting.all.sample, only: [:message]
   end
 end
